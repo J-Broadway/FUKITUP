ec23da12c8dff5a319335daf350a7cacdebd0fd2
J-Broadway <jeremiah.broadway@hotmail.com>
2023-11-10 13:51:42 -0600

    started python script

---
+++ FUKITUP.py
@@ -0,0 +1,38 @@
+import click
+import sys
+
+load_menu         = '| Load Media | Open Folder | Load Preset | Info | '
+load_menu_pointer = '  ^            ^             ^    ^        ^'
+
+@click.command()
+@click.option('--load_media', '-l')
+@click.option('--open_folder', '-o')
+@click.option('--load_preset', '-lp')
+@click.option('--info', '-i', is_flag=True)
+def startup(load_media, open_folder, load_preset, info):
+    if load_media:
+        print('load media')
+    if open_folder:
+        print('open folder')
+    if load_preset:
+        print(load_preset)
+    if info:
+        print('info')
+
+def main():
+    args = sys.argv[1:]  # Get command line arguments except the filename
+    try:
+        startup(standalone_mode=False)
+    except click.ClickException as e:
+        if '-lp' in args:
+            print("Error related to load preset option:", e.message)
+        elif '-o' in args:
+            print("Error related to open folder option:", e.message)
+        elif '-i' in args:
+            print("Error related to info option:", e.message)
+        else:
+            print("General Error:", e.message)
+
+
+if __name__ == '__main__':
+    main()
