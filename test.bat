28fdc34ac0618499f54a4c459f78201f1db922aa
Jeremiah Broadway (JBroadway) <46094056+J-Broadway@users.noreply.github.com>
2024-08-01 13:45:39 -0500

    Delete test.bat
    not needed

--- test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ^| Run It Back ^| Stack It ^| Combine It ^| New IMG ^| Save IMG ^| Load Preset ^| Save Preset ^| Create Sequence ^|
-echo   ^^             ^^     ^^    ^^            ^^         ^^          ^^    ^^        ^^    ^^        ^^
-
-pause
